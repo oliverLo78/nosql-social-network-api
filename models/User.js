78a26b2dd5acbdac7f6f2ed42cad0a31282478a9
oliverLo78 <oliverberto@gmail.com>
2022-11-25 16:09:41 -0500

    set up user.js model

--- models/User.js
+++ models/User.js
@@ -4,12 +4,18 @@ const { Schema, model } = require('mongoose');
 const userSchema = new Schema(
   {
     username: { type: String, required: true },
-    email: String,
-    age: Number,
-    posts: [
+    email: { type: String, required: true },
+    thoughts: [
+        {
+            // references the thought ObjectId
+            type: Schema.Types.ObjectId,
+            ref: 'thought'
+        },
+    ],
+    friends: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'thought',
+        ref: 'user',
       },
     ],
   },
@@ -25,18 +31,12 @@ const userSchema = new Schema(
 
 // Create a virtual property `fullName` that gets and sets the user's full name
 userSchema
-  .virtual('fullName')
+  .virtual('friendCount')
   // Getter
   .get(function () {
-    return `${this.first} ${this.last}`;
-  })
-  // Setter to set the first and last name
-  .set(function (v) {
-    const first = v.split(' ')[0];
-    const last = v.split(' ')[1];
-    this.set({ first, last });
+    return this.friends.length;
   });
-
+ 
 // Initialize our User model
 const User = model('user', userSchema);
 
